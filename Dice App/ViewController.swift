dafdaa23804a8092ed6116c022edbe200eeec64e
tin <tin.pt@ntnc.vn>
2023-06-06 04:40:27 +0700

    Initial Commit

---
+++ Dice App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by Administrator on 06/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
