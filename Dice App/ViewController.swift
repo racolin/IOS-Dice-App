72fe7760dcad8c846db94655853db58d643fac76
tin <tin.pt@ntnc.vn>
2023-06-11 23:46:17 +0700

    initial commit

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "dice_\(Int.random(in: 1...6)).png")
+        
+        diceImageView2.image = UIImage(imageLiteralResourceName: "dice_\(Int.random(in: 1...6)).png")
+        
+    }
 }
 
